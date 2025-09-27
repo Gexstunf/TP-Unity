e14e9b07b3161b4527768add855e7ac4b1cf1fd9
Gexstunf <166758362+Gexstunf@users.noreply.github.com>
2025-09-26 21:39:41 -0300

    Update RestartScene.cs

--- Assets/Scripts/RestartScene.cs
+++ Assets/Scripts/RestartScene.cs
@@ -5,13 +5,15 @@ public class RestartScene : MonoBehaviour
 {
     [SerializeField] private string sceneName = "Animated_Character_Navigation_Final";
 
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
     // Esta función se llama desde el botón UI
     public void RestartGame()
     {
-        // Desbloquea y muestra el cursor antes de cambiar de escena
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
-
         SceneManager.LoadScene(sceneName);
     }
 }
